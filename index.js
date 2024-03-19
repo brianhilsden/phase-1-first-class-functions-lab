6fa49b1031db102968614270ee1ff9c5e2bb99c7
Brian Omondi <brianhilsden@gmail.com>
2024-03-19 14:54:20 +0300

    worked on advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(array){
+
+    const newArray=array.slice(0,2);
+    return newArray;
+}
+
+function returnLastTwoDrivers(array){
+    const newArray=array.slice(array.length-2)
+    return newArray;
+}
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+    return function (fare){
+        return fare*num
+    }
+}
+
+function fareDoubler(fare){
+    const doubled=createFareMultiplier(2)(fare);
+    return doubled;
+}
+
+function fareTripler(fare){
+    const tripled=createFareMultiplier(3)(fare);
+    return tripled;
+}
+
+function selectDifferentDrivers(drivers,FirstOrLast){
+    return FirstOrLast(drivers)
+}
